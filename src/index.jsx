7307e155a38f5a7d905960a814ab3f076f20ffff
smith pham <phamngocvances@gmail.com>
2018-07-02 15:24:28 +0700

    aa
    test
    
    Co-Authored-By: Phạm Ngọc Văn <phamngocvances@gmail.com>

--- src/index.jsx
+++ src/index.jsx
@@ -1,15 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-// import { ConnectedRouter } from 'react-router-redux';
 import 'babel-polyfill';
 import 'mdn-polyfills/Object.assign';
 import '../public/styles/app.less';
 import AppRoutes from './router';
 import { store } from './configureStore';
-// import HomePage from './containers/homepage';
-// window.$ = require('jquery');
-// window.jQuery = require('jquery');
 const rootElement = document.getElementById('app');
 
 ReactDOM.render(
